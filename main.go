8cae00fdce6a1b70c43a77f5f4817ea2e6ee1896
Ackberry <akbari@usf.edu>
2026-08-31 15:59:56 -0400

    Set request headers after handling request creation errors

--- main.go
+++ main.go
@@ -114,10 +114,10 @@ func postGraphQL(ctx context.Context, query string, variables map[string]any, re
 		http.MethodPost,
 		leetcode,
 		bytes.NewBuffer(jsonBody))
-	req.Header.Set("Content-Type", "application/json")
 	if err != nil {
 		return err
 	}
+	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
